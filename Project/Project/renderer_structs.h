62b3c7c95c399ec2192b8cb7083a3bbc33568c2c
Wichet Manawanitjarern <wmanawanitjarern@fullsail.edu>
2018-10-11 01:48:11 -0400

    Add Dummy, Constant Buffer to Toggle Color in Pixel Shader (for color change after collision)

--- Project/Project/renderer_structs.h
+++ Project/Project/renderer_structs.h
@@ -1,13 +1,26 @@
+/************************************************************************
+* Filename:  		renderer_structs.h
+* Date:      		10/02/2018
+* Mod. Date: 		10/11/2018
+* Mod. Initials:	WM
+* Author:    		Wichet Manawanitjarern
+* Purpose:   		Additional structs to hold different datatype and make accessing them easier.
+*************************************************************************/
 #pragma once
 
 #include <DirectXMath.h>
 #include "basic_structs.h"
 
-struct tConstantBuffer
+struct tConstantBuffer_VertexShader_WVPC
 {
 	XMFLOAT4X4 fWorld_Matrix;
 	XMFLOAT4X4 fView_Matrix;
 	XMFLOAT4X4 fProjection_Matrix;
 	XMFLOAT4X4 fCamera_Matrix;
 	XMFLOAT4X4 fCamera_Origin;
+};
+
+struct tConstantBuffer_PixelShader_DUMMY
+{
+	XMFLOAT4 fToggle = { 0.0f, 0.0f, 0.0f, 0.0f };
 };
